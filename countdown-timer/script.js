8a3791b3fe62f5843b8d42b4bb685eca6e8839d4
Borja Lagarón <borjalagaron@gmail.com>
2020-12-21 20:52:07 +0100

    countdown ok

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,10 +1,29 @@
-const newYears = "1 Jan 2021";
+const monthsEl = document.getElementById("months");
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secsEl = document.getElementById("secs");
 
-function countDown() {
-  const newYearsDate = new Date(newYears);
+function countdown() {
   const currentDate = new Date();
+  const newYearsDate = new Date("1 Jan " + (currentDate.getFullYear()+1));
 
-  console.log(newYearsDate - currentDate);
+  const diffDate = newYearsDate - currentDate;
+
+  const months = Math.floor((diffDate / (1000 * 60 * 60 * 24 * 365)) % 12);
+  const days = Math.floor((diffDate / (1000 * 60 * 60 * 24)) % 365);
+  const hours = Math.floor((diffDate / (1000 * 60 * 60)) % 24);
+  const mins = Math.floor((diffDate / (1000 * 60)) % 60);
+  const secs = Math.floor((diffDate / 1000) % 60);
+
+  monthsEl.innerHTML = months;
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = mins;
+  secsEl.innerHTML = secs;
 }
 
-countDown();
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
