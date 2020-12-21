b5a1d0ab816a807ee9f2c261da86e12ee752e4f9
Borja Lagarón <borjalagaron@gmail.com>
2020-12-21 14:37:17 +0100

    css countdown

---
+++ countdown-timer/script.js
@@ -0,0 +1,10 @@
+const newYears = "1 Jan 2021";
+
+function countDown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+
+  console.log(newYearsDate - currentDate);
+}
+
+countDown();
